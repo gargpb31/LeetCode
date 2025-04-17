7599b3f1ba94478ddaac14ee154a581b8bab39aa
Sunny Garg <sunnygarg887@gmail.com>
2025-04-17 13:12:53 +0530

    Time: 6 ms (3.85%), Space: 16.8 MB (6.09%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -2,31 +2,62 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int k) 
     {
-        vector<vector<int>> v(101);
+        //optimal solution in O(nums.size()*(sqrt(k)));
 
-        for(int i=0; i<nums.size(); i++)
+        //Precompute divisors of k 
+
+        vector<int> divisors;
+
+        for(int i=2; i*i<=k; i++)
         {
-            v[nums[i]].push_back(i);
-        }
-
-        int ans = 0;
-
-        for(int i=0; i<=100; i++)
-        {
-            if(v[i].size()>=2)
+            if(k%i==0)
             {
-                int x = v[i].size();
-                for(int j=0; j<x; j++)
+                divisors.push_back(i);
+                if((k/i)!=i)
                 {
-                    for(int k1=j+1; k1<x; k1++)
-                    {
-                        int mul = v[i][j]*v[i][k1];
-                        if(mul%k==0) ans++;
-                    }
+                    divisors.push_back((k/i));
                 }
             }
         }
 
+        divisors.push_back(k);
+        if(k!=1) divisors.push_back(1);
+
+        //Now we will precomput for every value in nums[i] that how many index are there
+
+        unordered_map <int,vector<int> > m;
+
+        //Insert : O(1) average : O(n) : worst case
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            m[nums[i]].push_back(i); 
+        }
+
+        int ans = 0;
+
+        for(auto& [key,vec] : m)
+        {
+            unordered_map<int,int> mpp;
+
+            for(int i : vec)
+            {
+                int gcdd = gcd(i,k);
+                int need = k/gcdd;
+
+                ans+=mpp[need];
+
+                for(int j : divisors)
+                {
+                    if(i%j==0)
+                    {
+                        mpp[j]++;
+                    }
+                }
+            }
+        }
         return ans;
+
+
     }
 };
