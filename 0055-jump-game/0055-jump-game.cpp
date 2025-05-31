414b4fab8b3d2e552b1c126e57c4c4158e416ee8
Sunny Garg <sunnygarg887@gmail.com>
2025-05-31 11:03:57 +0530

    Time: 0 ms (100%), Space: 52.3 MB (33.89%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int g=0;
-        for(auto it : nums){
-            if((g+it)>=nums.size()-1) return true;
-            g++;
+        int start = 0;
+        int l = 0;
+        int n = nums.size();
+        while(l<n-1 && start<=l)
+        {
+            int y = nums[start];
+            l=max(l,start+y);
+            start++;
         }
+        if(l>=(n-1)) return true;
         return false;
+
     }
 };
