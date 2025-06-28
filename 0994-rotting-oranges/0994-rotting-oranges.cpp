d0710d0d1f7ab814d85f28621f44465dd10b61f5
Sunny Garg <sunnygarg887@gmail.com>
2025-06-28 10:21:47 +0530

    Time: 0 ms (100%), Space: 16.7 MB (75.26%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,68 +1,59 @@
 class Solution {
 public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
 
-   
-    int orangesRotting(vector<vector<int>>& v) {
-       queue<pair<pair<int,int> ,int> > q;
-        int n =v.size();
-        int m = v[0].size();
-                for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(v[i][j]==2) {
-                   q.push({{i,j},0});
+        int time = 0;
+
+        queue<pair<int,pair<int,int>>> q;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j]==2)
+                {
+                    q.push({0,{i,j}});
                 }
             }
         }
-        int cnt =0;
-               
-               int ans =0;
-            
-                while(!q.empty()){
-                   
-                        
-                   
-                    int x = q.front().first.first;
-                    int y = q.front().first.second;
-                    int z = q.front().second;
-                    q.pop();
-                    z++;
-                    if((x+1)<n && v[x+1][y]==1){
-                        v[x+1][y]=2;
-                        ans=max(ans,z);
-                        q.push({{x+1,y},z});
-                    }
-                    if((x-1) >=0 && v[x-1][y]==1){
-                        v[x-1][y]=2;
-                            q.push({{x-1,y},z});
-                            ans=max(ans,z);
-                    }
-                    if(y+1<m && v[x][y+1]==1){
-                        v[x][y+1]=2;
-                            q.push({{x,y+1},z});
-                            ans=max(ans,z);
-                    }
-                    if(y-1>=0 && v[x][y-1]==1){
-                        v[x][y-1]=2;
-                            q.push({{x,y-1},z});
-                            ans=max(ans,z);
-                    }
-               
-                    
 
-                }
-        
-            int flag=0;
-            
- for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(v[i][j]==1) {
-                    flag=1;
+        int ans = 0;
+        while(!q.empty())
+        {
+            int curtime = q.front().first;
+            int x1 = q.front().second.first;
+            int y1 = q.front().second.second;
+            q.pop();
+            int col[] = {-1,0,+1,0};
+            int row[] = {0,1,0,-1};
+
+            for(int i=0; i<4; i++)
+            {
+                int nr = x1+row[i];
+                int nc = y1+col[i];
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m)
+                {
+                    if(grid[nr][nc]==1)
+                    {
+                        q.push({curtime+1,{nr,nc}});
+                        grid[nr][nc]=2;
+                        ans=max(ans,curtime+1);
+                    }
                 }
             }
- }
+        }
 
- if(flag==1) return -1;
- return ans;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return ans;
 
     }
 };
