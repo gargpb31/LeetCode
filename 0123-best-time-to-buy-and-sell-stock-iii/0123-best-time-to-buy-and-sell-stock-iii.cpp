f34a28084293264037bd7cfab62ca541237b7637
Sunny Garg <sunnygarg887@gmail.com>
2025-06-29 09:33:25 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,53 @@
 class Solution {
 public:
 
-    int ans(vector<int> &prices, int n, int d, int flag,vector<vector<vector<int>>> &dp)
-    {
-            if(d==0) return 0;
-            if(n==prices.size()) return 0;
-        if(dp[n][flag][d]!=-1) return dp[n][flag][d];
-            if(flag>0)
-            {
-return dp[n][flag][d]=max(prices[n]+ans(prices,n+1,d-1,0,dp),ans(prices,n+1,d,1,dp));
-            }
+//     int ans(vector<int> &prices, int n, int d, int flag,vector<vector<vector<int>>> &dp)
+//     {
+//             if(d==0) return 0;
+//             if(n==prices.size()) return 0;
+//         if(dp[n][flag][d]!=-1) return dp[n][flag][d];
+//             if(flag>0)
+//             {
+// return dp[n][flag][d]=max(prices[n]+ans(prices,n+1,d-1,0,dp),ans(prices,n+1,d,1,dp));
+//             }
 
-        return dp[n][flag][d]=max(-prices[n]+ans(prices,n+1,d,1,dp),ans(prices,n+1,d,0,dp));
+//         return dp[n][flag][d]=max(-prices[n]+ans(prices,n+1,d,1,dp),ans(prices,n+1,d,0,dp));
         
-    }
+//     }
 
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
 
-vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
 
-    return ans(prices,0,2,0,dp);
+    for(int i=n-1; i>=0; i--)
+    {
+        for(int flag=0; flag<2; flag++)
+        {
+            for(int d=1; d<3; d++)
+            {
+                if(flag>0)
+                {
+            dp[i][flag][d]=max(prices[i]+dp[i+1][0][d-1],dp[i+1][1][d]);
+                }
+                else
+                {
+            dp[i][flag][d]=max(-prices[i]+dp[i+1][1][d],dp[i+1][0][d]);
+                }
+            }
+        }
+    }
+    int ans = 0;
+    for(int flag=0; flag<2; flag++)
+    {
+        for(int d=1; d<=2; d++)
+        {
+                ans=max(ans,dp[0][flag][d]);
+        }
+    }
+    return ans;
 
     }
 };
