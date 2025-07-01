20541153b5ddd121471ed86a182b54485c588dcf
Sunny Garg <sunnygarg887@gmail.com>
2025-07-01 19:33:07 +0530

    Time: 18 ms (9.81%), Space: 18.2 MB (56.8%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
-        vector<vector<pair<int, int>>> graph(n);
-        for (int i = 0; i < flights.size(); i++) {
-            graph[flights[i][0]].push_back({flights[i][1], flights[i][2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> graph(n);
+        int f = flights.size();
+
+        for(int i=0; i<f; i++)
+        {
+        graph[flights[i][0]].push_back({flights[i][1],flights[i][2]});
         }
 
-        vector<int> ans(n,1e9);
-       ans[src]=0;
-        set<pair<int, pair<int, int>>> s;
-        s.insert({0, {src, 0}});
+priority_queue<pair<pair<int,int>,int>, vector<pair<pair<int,int>,int>>, greater<pair<pair<int,int>,int>>> pq;
 
-        while (!s.empty()) {
-            auto it = s.begin();
-            int x = (*it).first;
-            int y = (*it).second.first;
-            int z = (*it).second.second;
-
-            s.erase(it);
-
-            for (auto it2 : graph[y]) {
-                int nn = z + it2.second;
-            
-                    if (x <= k) {
-                        if (ans[it2.first] > nn) {
-                            ans[it2.first] = nn;
-                            s.insert({x+1, {it2.first, nn}});
-                        }
-                    }
-                
-            }
-        }
+   vector<vector<int>> dist(n, vector<int>(k+2, 1e8));
+dist[src][0] = 0;
+  
+    pq.push({{0,0},src});
+   
+    while(!pq.empty())
+    {
+        auto p = pq.top();
+        pq.pop();
         
-        if (ans[dst] == 1e9)
-            return -1;
-        return ans[dst];
+
+            if (p.first.second > k) continue;
+        for(auto it : graph[p.second])
+        {
+            int nv = p.first.first+it.second;
+            if (dist[it.first][p.first.second + 1] > nv) {
+    dist[it.first][p.first.second + 1] = nv;
+    pq.push({{nv, p.first.second + 1}, it.first});
+}
+        }
+    }
+    int minCost = *min_element(dist[dst].begin(), dist[dst].end());
+        return minCost == 1e8 ? -1 : minCost;
+               
     }
 };
