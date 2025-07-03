519875bd5ba09adbaa01c5a1cb69ce768de57825
Sunny Garg <sunnygarg887@gmail.com>
2025-07-03 14:35:31 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.99%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,23 +12,48 @@
 class Solution {
 public:
 
-    bool issame(TreeNode* p,TreeNode* q)
-    {
-        if(p==NULL && q==NULL) return true;
-        if(p==NULL || q==NULL) return false;
-
-        if(p->val!=q->val) return false;
-
-        bool left = issame(p->left,q->left);
-        bool right = issame(p->right,q->right);
-
-        return left|right;
-    }
+    
 
 
 
 
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        return issame(p,q);
+       if(p==NULL && q==NULL) return true;
+       if(p==NULL || q==NULL) return false;
+
+       if(p->val!=q->val) return false;
+
+       stack<TreeNode*> s1,s2;
+
+       s1.push(p);
+       s2.push(q);
+
+       while(!s1.empty())
+       {
+        p=s1.top();
+        q=s2.top();
+        s1.pop();
+        s2.pop();
+
+            if(p->val!=q->val) return false;
+
+            if(p->right==NULL && q->right==NULL);
+            else if(p->right==NULL || q->right==NULL) return false;
+            else
+            {
+                s1.push(p->right);
+                s2.push(q->right);
+            }
+
+
+            if(p->left==NULL && q->left==NULL);
+            else if(p->left==NULL || q->left==NULL) return false;
+            else
+            {
+                s1.push(p->left);
+                s2.push(q->left);
+            }
+       }
+       return true;
     }
 };
