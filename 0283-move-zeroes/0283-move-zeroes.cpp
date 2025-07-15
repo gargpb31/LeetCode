d17a59998d034777e75c332bfb0638c7468c9178
Sunny Garg <sunnygarg887@gmail.com>
2025-07-15 16:24:48 +0530

    Time: 3 ms (17.78%), Space: 23.8 MB (52.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int g = 0;
         int n = nums.size();
 
+        if(n==1) return;
+
+        int ze = -1;
+        int nze =-1;
         for(int i=0; i<n; i++)
         {
-            if(nums[i]!=0)
+            if(nums[i]==0)
             {
-                swap(nums[i],nums[g]);
-                g++;
+                if(ze==-1)
+                ze=i;
             }
+            else
+            {
+                nze=i;
+            }
+
+            if(ze<nze && ze!=-1) 
+            {
+                swap(nums[ze],nums[nze]);
+                ze++;
+                nze=-1;
+
+            }
+
         }
-      
     }
 };
