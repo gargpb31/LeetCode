4d47df8ac105aff67480b89ca664013316185f33
Sunny Garg <sunnygarg887@gmail.com>
2025-07-16 09:46:44 +0530

    Time: 37 ms (15.93%), Space: 48.8 MB (23.36%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,53 +1,95 @@
+class DSU{
+    public : vector<int> parent,rank;
+
+    DSU(int size)
+    {
+        parent.resize(size);
+        rank.resize(size,1);
+
+        for(int i=0; i<size; i++)
+        {
+            parent[i]=i;
+        }
+    }
+
+    int findpar(int node)
+    {
+        if(parent[node]==node) return node;
+
+        return parent[node]=findpar(parent[node]);
+    }
+
+    void unite(int node1,int node2)
+    {
+        int parent1 = findpar(node1);
+        int parent2 = findpar(node2);
+
+        if(parent1==parent2)
+        {
+            return;
+        }
+
+        if(rank[parent1]<rank[parent2])
+        {
+            parent[parent1]=parent2;
+
+        }
+        else if (rank[parent1]>rank[parent2])
+        {
+            parent[parent2]=parent1;
+        }
+        else
+        {
+            parent[parent1]=parent2;
+            rank[parent2]++;
+        }
+    }
+
+
+
+};
+
+
+
 class Solution {
 public:
-
-
-    int find(vector<int> &parent, int i){
-		if(parent[i]==i) return i;
-		parent[i]=find(parent,parent[i]);
-		return parent[i];
-	
-	}
-
     int makeConnected(int n, vector<vector<int>>& connections) {
-        	vector<int> parent(n);
-	vector<int> rank(n,0);
-	for(int i=0; i<n; i++) parent[i]=i;
-	
-	
-	int freedge=0;
+        
+        int extraedges = 0;
+        DSU d (n);
+        for(auto it : connections)
+        {
+            int computer1 = it[0];
+            int computer2 = it[1];
 
-	for(auto it : connections){
-int rootf = find(parent,it[0]);
-int roots= find(parent,it[1]);
+            int par_computer1 = d.findpar(computer1);
+            int par_computer2 = d.findpar(computer2);
 
-if(rootf==roots) freedge++;
-else {
-	if(rank[rootf]>rank[roots]) parent[roots]=rootf;
-	else if (rank[rootf]<rank[roots]) parent[rootf]=roots;
-	else {
-	parent[rootf]=roots;
-	rank[rootf]++;
-}
+            if(par_computer1==par_computer2)
+            {
+                extraedges++;
+            }
+            else
+            {
+                d.unite(computer1,computer2);
+            }
+        }
 
-}
-	
-	}
+        map<int,int> m;
 
-	
-	for(int i=0; i<n; i++) {
-		parent[i]=find(parent,i);
-	}
-	
-	int components=0;
+        for(int i=0; i<n; i++)
+        {
+            int par_computer = d.findpar(i);
+            m[par_computer]++;
+        }
+        int components = m.size();
+        cout<<components;
 
-	for(int i=0; i<n; i++){
-if(parent[i]==i) components++;
-	
-	}
-
-	if(components-1<=freedge) return components-1;
-	return -1;
+        if(extraedges>=(components-1))
+        {
+            return components-1;
+        }
+        return -1;
 
     }
 };
