fd056b459d9ba025a3ddb7089b159c4c2901757e
Sunny Garg <sunnygarg887@gmail.com>
2025-12-18 23:05:35 +0530

    Time: 16 ms (43.08%), Space: 328.3 MB (22.98%) - LeetHub

--- 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
+++ 3652-best-time-to-buy-and-sell-stock-using-strategy/3652-best-time-to-buy-and-sell-stock-using-strategy.cpp
@@ -3,9 +3,8 @@ public:
     long long maxProfit(vector<int>& prices, vector<int>& strategy, int k) {
         int n = prices.size();
         vector<long long> add(n);
-
         long long x = 0;
-         vector<long long> add2(n);
+        vector<long long> add2(n);
         long long y= 0;
         for(int i=0; i<n; i++)
         {
@@ -15,21 +14,14 @@ public:
             add2[i]=y;
         }
         long long ans=y;
-        
-        
         for(int i=0; i<=(n-k); i++)
         {
-           
             long long r = add2[i+k-1];
             if(i!=0) r-=add2[i-1];
             long long r2 = add[i+k-1]-add[i+(k/2-1)];
             long long z = y-r+r2;
             ans=max(ans,z);
         }
-        return ans;
-
-
-
-        
+        return ans;    
     }
 };
