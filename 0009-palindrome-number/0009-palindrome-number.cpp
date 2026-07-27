1d1b5200b0343c9ecccddb106fb9aafbb70690aa
Sunny Garg <sunnygarg887@gmail.com>
2026-07-27 11:01:44 +0530

    Time: 5 ms (19.39%), Space: 10.9 MB (12.6%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,18 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
        
-        if(x<0) return false;
-        int s = x;
-        long long rev = 0;
-        while(s!=0)
-        {
-            rev=rev*10+s%10;
-            s/=10;
-
-        }
-        
-        if(x==rev) return true;
-        return false;
+       if(x<0) return false;
+       string s = to_string(x);
+       string s1 = s;
+        reverse(s.begin(),s.end());
+       if(s==s1) return true;
+       return false;
 
     }
 };
