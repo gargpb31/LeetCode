7b0e5bef54ffe48cdeb919ceb16edb2d28883793
Sunny Garg <sunnygarg887@gmail.com>
2026-08-02 11:16:37 +0530

    Time: 23 ms (52.53%), Space: 70.4 MB (80.32%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,58 +1,30 @@
 class Solution {
 public:
-
-    bool check(vector<int>& bloomDay, int mid, int k, int m)
-    {
-        int n = bloomDay.size();
-
-        vector<int> v(n,0);
-
-        for(int i=0; i<n; i++)
-        {
-            if(bloomDay[i]<=mid) v[i]=1;
-        }
-
-        int count = 1;
-        int y = 0;
-        for(int i=0; i<n-1; i++)
-        {
-            if(v[i]==v[i+1] && v[i]==1) count++;
-            else
-            {
-                if(v[i]==1)
-                {
-                        y+=(count/k);
-                }
-                count=1;
-            }
-        }
-        if(v[n-1]==1)
-        {
-             y+=(count/k);
-        }
-
-        if(y>=m) return true;
-        return false;
-
-    }
-
-    int minDays(vector<int>& bloomDay, int m, int k) 
-    {
-        int low =1;
+    int minDays(vector<int>& bloomday, int m, int k) {
+        int n = bloomday.size();
         int high = 1e9;
+        int low = 1;
 
         while(low<=high)
         {
-            int mid = low+(high-low)/2;
+            int mid = (low+high)/2;
+            int c = 0;
+            int c2=0;
+            for(int i=0; i<n; i++)
+            {
+                if(bloomday[i]<=mid)
+                {
+                    c2++;
+                    if(c2==k) {c++; c2=0;}
+                }
+                else
+                {
+                    c2=0;
+                }
+            }
+            if(c>=m) high=mid-1;
+            else low=mid+1;
 
-            if(check(bloomDay,mid,k,m))
-            {
-                high=mid-1;
-            }
-            else
-            {
-                low=mid+1;
-            }
         }
         if(low>1e9) return -1;
         return low;
