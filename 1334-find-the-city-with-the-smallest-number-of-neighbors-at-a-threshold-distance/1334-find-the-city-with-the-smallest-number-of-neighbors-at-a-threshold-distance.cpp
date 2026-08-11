b6ad3c8bceaddc061c7b99acc03d0fb8bc23344f
Sunny Garg <sunnygarg887@gmail.com>
2026-08-11 23:32:09 +0530

    Time: 17 ms (41.1%), Space: 17.1 MB (38.48%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> matrix(n,vector<int>(n,1e9));
-        int e = edges.size();
+    int findTheCity(int n, vector<vector<int>>& edges, int dist) {
+        vector<vector<int>> v(n,vector<int>(n,1e9));
 
-        for(int i=0; i<e; i++)
+      for(int i = 0; i < n; i++)
+
+            v[i][i] = 0;
+
+        for(auto it : edges)
         {
-            matrix[edges[i][0]][edges[i][1]]=edges[i][2];
-            matrix[edges[i][1]][edges[i][0]]=edges[i][2];
-        }
-        for(int i=0; i<n; i++)
-        {
-            matrix[i][i]=0;
+            v[it[0]][it[1]]=it[2];
+            v[it[1]][it[0]]=it[2];
         }
 
         for(int k=0; k<n; k++)
@@ -20,31 +19,37 @@ public:
             {
                 for(int j=0; j<n; j++)
                 {
-                matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
+                    if(v[i][k]==1e9 || v[j][k]==1e9) continue;
+
+                    if(v[i][j]==1e9)
+                    {
+                        v[i][j]=v[i][k]+v[k][j];
+                    }
+                    else
+                    {
+                        v[i][j]=min(v[i][j],v[i][k]+v[k][j]);
+                    }
                 }
             }
         }
-        int ans = 0;
-        int c = n;
+
+        vector<int> c(n);
         for(int i=0; i<n; i++)
         {
-            int count = 0;
             for(int j=0; j<n; j++)
             {
-               if(matrix[i][j]<=distanceThreshold && i!=j) count++;
+                if(v[i][j]<=dist)
+                {
+                    c[i]++;
+                }
             }
-            cout<<i<<" "<<count<<endl;
-            if(count<c)
-            {
-                c=count;
-                ans=i;
-            }
-            else if(c==count)
-            {
-                ans=max(ans,i);
-            }
-
         }
-        return ans;
+
+        int dd = *min_element(c.begin(),c.end());
+        for(int i=n-1; i>=0; i--)
+        {
+            if(c[i]==dd) return i;
+        }
+        return -1;
     }
 };
