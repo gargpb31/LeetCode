38225a1231883e30eedd6a33f501343853f2cf42
Sunny Garg <sunnygarg887@gmail.com>
2026-08-12 22:34:38 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
 
-    int ways(vector<int> &dp,int pos,int n)
-    {
-        if(pos==n) return 1;
-        if(pos>n) return 0;
+    // int ways(vector<int> &dp,int pos,int n)
+    // {
+    //     if(pos==n) return 1;
+    //     if(pos>n) return 0;
 
-        if(dp[pos]!=-1) return dp[pos];
+    //     if(dp[pos]!=-1) return dp[pos];
 
-        int totways = ways(dp,pos+1,n)+ways(dp,pos+2,n);
-        return dp[pos]=totways;
-    }
+    //     int totways = ways(dp,pos+1,n)+ways(dp,pos+2,n);
+    //     return dp[pos]=totways;
+    // }
 
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
 
-        int ans = ways(dp,0,n);
-        return ans;
+        dp[0]=1;
+        dp[1]=1;
+      
+
+        for(int i=2; i<=n; i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+
+       
     }
 };
