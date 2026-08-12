66631d928869eaf52a50df854fdc56ba3a56958d
Sunny Garg <sunnygarg887@gmail.com>
2026-08-12 23:44:07 +0530

    Time: 219 ms (15.46%), Space: 263.6 MB (6.03%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
-map<int,int> m;
-
-bool ans(int jump, vector<vector<int>> &dp,int n,int end)
+    bool ans(map<int,int>&m, int cur, vector<vector<int>> &dp, int k,int end)
     {
-        if(n==end) return true;
-        if(m.find(n)==m.end()) return false;
-
-        if(dp[m[n]][jump]!=-1) return dp[m[n]][jump];
-
-        bool k1 = ans(jump+1,dp,n+jump+1,end);
-        bool k2 = ans(jump,dp,n+jump,end);
-        bool ans1 = k1|k2;
-        if(jump!=1)
+        if(cur==end) return true;
+        if(k >= dp.size()) return false;
+        if(dp[m[cur]][k]!=-1)
         {
-            bool k3 = ans(jump-1,dp,n+jump-1,end);
-            ans1=ans1|k3;
+            return dp[m[cur]][k];
         }
-        return dp[m[n]][jump]=ans1;
+        bool fina = false;
+        int cur1 = cur+k;
+        if(k>0)
+        {
+        if(m.find(cur1)!=m.end())
+        {
+            bool k1 = ans(m,cur1,dp,k,end);
+            fina=fina|k1;
+        }
+        }
+        int cur2 = cur+k+1;
+        
+        if(m.find(cur2)!=m.end())
+        {
+            bool k2 = ans(m,cur2,dp,k+1,end);
+            fina=fina|k2;
+        }
+        if(k>1){
+        int cur3 = cur+k-1;
+            if(m.find(cur3)!=m.end())
+            {
+                bool k3 = ans(m,cur3,dp,k-1,end);
+                fina=fina|k3;
+            }
+
+        }
+       return dp[m[cur]][k]=fina;
 
     }
 
 
     bool canCross(vector<int>& stones) {
         int n = stones.size();
+        map<int,int> m;
         for(int i=0; i<n; i++) m[stones[i]]=i;
+
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        bool ans1 = ans(m,0,dp,0,stones[n-1]);
+        return ans1;
         
-        
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        return ans(1,dp,1,stones[n-1]);
 
     }
 };
